ac4d4403f04b90085f2582e6dd29e32d336e85dd
Wang-Zhou Dai <dai.wzero@gmail.com>
2021-02-10 11:57:15 +0800

    update js

---
+++ org-html-themes/styles/readtheorg/js/mathjax-config.js
@@ -0,0 +1,12 @@
+window.MathJax = {
+    loader: {
+        load: ['[tex]/colorv2']
+    },
+    tex: {
+        packages: {'[+]': 'colorv2'},
+        autoload: {color: []}
+    },
+    svg: {
+        fontCache: 'global'
+    }
+};
